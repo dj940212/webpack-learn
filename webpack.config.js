613e280881c422c1ec9c732c8d187ee6367be7fd
dj940212 <2902273280@qq.com>
2018-03-26 21:53:22 +0800

    完成webpack编译ES6

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: {
+        app: './app.js'
+    },
+    output: {
+        filename: '[name].[hash:8].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    }
+}
